2b00680611bb9e4a19097bccd6a956171ad3ce09
Bryan Urias <bryanurias@Bryans-Air.attlocal.net>
2020-11-29 13:47:14 -0600

    working on frontpage css

--- src/Components/Landing Page/LandingRowTwo.js
+++ src/Components/Landing Page/LandingRowTwo.js
@@ -1,31 +1,43 @@
 import React from 'react'
 import "./LandingPage.css"
+import "./LandingRowOne.css"
+import { Jumbotron, Container, Row, Col } from "react-bootstrap";
+import interview from "../../assets/interview.png"
+
+
 
 function LandingRowTwo() {
     return (
-       
-        <div className="team" class="offset" >
-            <div className="fixed-background LandingRowTwo" id="light">
-                <div className="row light text-center">
+        <div >
+      
+        <Jumbotron expand="" className="Jumbotron" style={{backgroundColor: "#f1f3f5"}}>
+         
+        <Container className="container">
+        <h1 className="display-4">Why our Job Tracker?</h1>
+           <Row  className="row">
+           
+                <Col sm="4" className="col">
+               <h2 className="headertwo">
+                   Build by students, For students.
+               </h2>
+               <p>
+                   Having gone through our own steps of applying for positions, we found that many lack a clear and easy-to-use tracker to keep them "on track"
+               </p>
+                </Col>
+              
+            </Row> 
+        </Container>
+           
+        <img className="LandingRowOne_Image d-none d-lg-block" src={interview} alt="trying to get it to load"/>
+        
+  
 
-                    <div className="col-12 text-center">
-                        <h3 className="heading">Team</h3>
-                    </div>
 
 
-
-                </div>
-
-                <div class="fixed-wrap">
-                    <div class="fixed">
-
-                    </div>
-                </div>
-            </div>
-        </div>
+        </Jumbotron>
 
 
-
+    </div>
     )
 }
 
